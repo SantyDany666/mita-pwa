293a56c37dc5a704a3dbe2594239a59bc54c12b8
Santiago Gil <santigil77@gmail.com>
2026-01-19 23:10:07 -0500

    fix(create-profile): fix darkmode in header

--- src/features/create-profile/components/CreateProfileHeader.tsx
+++ src/features/create-profile/components/CreateProfileHeader.tsx
@@ -22,14 +22,16 @@ export const CreateProfileHeader = ({
       <div className="flex items-center justify-between mb-6 h-10">
         {isFirstStep ? (
           <>
-            <span className="text-xs font-semibold uppercase tracking-widest text-[#3E7CB1]">
+            <span className="text-xs font-semibold uppercase tracking-widest text-[#3E7CB1] dark:text-[#81A4CD]">
               Paso {step} de {totalSteps}
             </span>
             <div className="flex gap-1">
               {Array.from({ length: totalSteps }).map((_, i) => (
                 <div
                   key={i}
-                  className={`h-1.5 rounded-full ${i + 1 === step ? "w-10 bg-[#054A91]" : "w-6 bg-[#81A4CD]"
+                  className={`h-1.5 rounded-full ${i + 1 === step
+                      ? "w-10 bg-[#054A91] dark:bg-[#3E7CB1]"
+                      : "w-6 bg-[#81A4CD] dark:bg-slate-700"
                     }`}
                 />
               ))}
@@ -45,14 +47,16 @@ export const CreateProfileHeader = ({
               <ArrowLeft className="text-[#054A91] dark:text-[#81A4CD] w-6 h-6" />
             </button>
             <div className="flex flex-col items-end">
-              <span className="text-xs font-semibold text-[#3E7CB1] uppercase tracking-widest mb-1">
+              <span className="text-xs font-semibold text-[#3E7CB1] dark:text-[#81A4CD] uppercase tracking-widest mb-1">
                 Paso {step} de {totalSteps}
               </span>
               <div className="flex gap-1">
                 {Array.from({ length: totalSteps }).map((_, i) => (
                   <div
                     key={i}
-                    className={`h-1.5 rounded-full ${i + 1 === step ? "w-10 bg-[#054A91]" : "w-6 bg-[#81A4CD]"
+                    className={`h-1.5 rounded-full ${i + 1 === step
+                        ? "w-10 bg-[#054A91] dark:bg-[#3E7CB1]"
+                        : "w-6 bg-[#81A4CD] dark:bg-slate-700"
                       }`}
                   />
                 ))}
